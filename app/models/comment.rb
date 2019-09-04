12672fc73294462490b5bd669b481dd76b918fef
silverbear925 <s925maru.k.k@gmail.com>
2019-09-04 14:53:19 +0900

    add5

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+
+  validates :content, presence: true, unless: :image?
 end
--- db/migrate/20190904033339_create_comments.rb
+++ db/migrate/20190904033339_create_comments.rb
@@ -1,9 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string :body
+      t.string :content
       t.string :image
-      t.string :user,
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
