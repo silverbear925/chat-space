ab97962c79e3af7427169705af7c18bf9efd126d
silverbear925 <s925maru.k.k@gmail.com>
2019-09-04 15:48:18 +0900

    add6

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages_controller, only: [:index]
+    resources :messages, only: [:index]
 end
 end
