3eb259fe0ea16e77ed4c1fb218d646c58600eed3
silverbear925 <s925maru.k.k@gmail.com>
2019-09-10 21:17:41 +0900

    add8

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = ` <div class= "message" "data-message-id"=${message.id}>
+    var html = ` <div class= "message" data-message-id=${message.id}>
                   <div class= upper-message>
                     <div class= upper-message__user-name>
                       ${message.user_name}
@@ -48,7 +48,6 @@ $('#new_message').on('submit', function(e){
       $('.messages').append(html);
       $("#new_message")[0].reset();
       scrollBottom();
-      
     })
 
     .fail(function(data){
@@ -57,5 +56,31 @@ $('#new_message').on('submit', function(e){
     .always(() => {
       $('.form__submit').removeAttr("disabled");
     });
-  })
+  });
+
+ var reloadMessages = function () {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data("message-id");
+
+    $.ajax({ 
+      url: "api/messages",
+      type: 'get', 
+      dataType: 'json', 
+      data: {last_id: last_message_id} 
+    })
+    .done(function (messages) { 
+      // console.log(messages)
+      var insertHTML = '';
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
+    })
+    .fail(function () {
+      alert('自動更新に失敗しました');
+    });
+  }
+};
+setInterval(reloadMessages, 5000);
 })
