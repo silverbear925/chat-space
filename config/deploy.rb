a160bbaed8128dade337d9c17eea666582af62e2
silverbear925 <s925maru.k.k@gmail.com>
2019-09-13 02:18:37 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,8 +41,10 @@ set :repo_url, 'git@github.com:silverbear925/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/s925marukimiaws.pem']
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
